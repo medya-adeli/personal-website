9e0ca3af0adecd006ce08b207bd59dfa0772c34d
adolf <midiaadeli12@gmail.com>
2023-01-22 00:12:00 +0330

    push

---
+++ my-project/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: ['class', '[data-mode="dark"]'],
+}
