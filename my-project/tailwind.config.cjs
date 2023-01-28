58a7efc6bfded7c8c481a535970d3cc605b639e9
adolf <midiaadeli12@gmail.com>
2023-01-29 02:17:29 +0330

    push

--- my-project/tailwind.config.cjs
+++ my-project/tailwind.config.cjs
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
+      keyframes:{
+        wave:{
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation:{
+        'waiving-hand': 'wave  2s linear infinite',
+      },
       colors:{
         primary:"#FFD500"
       },
